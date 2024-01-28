f37323555d1570f10902801ccf777a1e510a0b9f
System Administrator <root@Blessings-MacBook-Pro.local>
2024-01-28 07:47:32 -0500

    configuring s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"  # region hardcoded
 }
 
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state"
+    key            = "devops-exercise/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "kingsley-bucket-test01" 
   acl    = "private"  # Access control list, options: private, public-read, public-read-write, authenticated-read
