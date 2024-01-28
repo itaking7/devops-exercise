19b033762bd3df176370e8937d0cf50a71fbdd4c
root <root@ip-172-31-46-111.ec2.internal>
2024-01-28 05:57:19 +0000

    updating the terraforn config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,14 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired region
+  region = "us-east-1"  # region hardcoded
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Replace with your desired bucket name
+  bucket = "kingsley-bucket-test01" 
   acl    = "private"  # Access control list, options: private, public-read, public-read-write, authenticated-read
   tags = {
     Name        = "MyBucket"
     Environment = "Production"
   }
 
-  # Uncomment the following if you want to enable versioning
-  # versioning {
-  #   enabled = true
-  # }
-  
-  # Uncomment the following if you want to enable server-side encryption
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       sse_algorithm = "AES256"
-  #     }
-  #   }
-  # }
 }
 
