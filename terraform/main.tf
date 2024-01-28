0dbb4e30dd2ad9d4dc8b352ab241bf47631a8531
System Administrator <root@Blessings-MacBook-Pro.local>
2024-01-28 12:49:26 -0500

    updating bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
+    bucket         = "terraform-state-kingsley"
     key            = "devops-exercise/terraform.tfstate"
     region         = "us-east-1"
   }
 }
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "kingsley-bucket-test01" 
+  bucket = "kingsley-bucket-test001" 
   acl    = "private"  # Access control list, options: private, public-read, public-read-write, authenticated-read
   tags = {
     Name        = "MyBucket"
