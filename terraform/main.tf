7fb4e2274ae46372921ed4ee6f5e61c5a6c3ccd7
System Administrator <root@Blessings-MacBook-Pro.local>
2024-01-29 12:07:36 -0500

    Deploying containers with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
   region = "us-east-1"  # region hardcoded
 }
 
@@ -19,4 +19,50 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 
 }
+*/
+
+# Define Docker provider
+provider "docker" {
+  host = "tcp://172.31.46.111:2375/"
+}
+
+# Define Docker containers
+resource "docker_container" "redis_container" {
+  image = "dominionsystemsinc/redis-devops-exercise"
+  name  = "redis_container"
+  ports {
+    internal = 6379
+    external = 6379
+  }
+}
+
+resource "docker_container" "back_container" {
+  image        = "dominionsystemsinc/backend-devops-exercise"
+  name         = "back_container"
+  ports {
+    internal = 4000
+    external = 4000
+  }
+  env = {
+    REDIS_SERVER = "redis_container:6379"
+  }
+  depends_on = [docker_container.redis_container]
+}
+
+resource "docker_container" "front_container" {
+  image        = "dominionsystemsinc/frontend-devops-exercise"
+  name         = "front_container"
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  env = {
+    BACKEND_API_URL = "http://back_container:4000"
+    CLIENT_API_URL  = "http://back_container:4000"
+  }
+  depends_on = [docker_container.back_container]
+}
+
+
+
 
